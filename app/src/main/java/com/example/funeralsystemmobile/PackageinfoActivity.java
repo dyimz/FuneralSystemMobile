ccd47afde408b7af7450aba5f07d8da062d0c375
James Christian Villamin <jameschristianvillamin@gmail.com>
2024-01-22 00:07:25 +0800

    working PACKAGE INFO and INCLUSIONS

--- app/src/main/java/com/example/funeralsystemmobile/PackageinfoActivity.java
+++ app/src/main/java/com/example/funeralsystemmobile/PackageinfoActivity.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -133,6 +135,15 @@ public class PackageinfoActivity extends AppCompatActivity {
         } else {
             Toast.makeText(getApplicationContext(), "Error Fetching Data", Toast.LENGTH_SHORT).show();
         }
+
+        Button AddToCart = findViewById(R.id.Inquire);
+        AddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                    Toast.makeText(getApplicationContext(), productId, Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), scustid, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
