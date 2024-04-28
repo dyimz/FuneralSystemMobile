e308c9ece0e8b5c0ccc98e4f213238b41129d00e
James Christian Villamin <jameschristianvillamin@gmail.com>
2024-04-29 06:46:17 +0800

    OBITUARIES

---
+++ app/src/main/java/com/example/funeralsystemmobile/ObituariesActivity.java
@@ -0,0 +1,14 @@
+package com.example.funeralsystemmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObituariesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obituaries);
+    }
+}
